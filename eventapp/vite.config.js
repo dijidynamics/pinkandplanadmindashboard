b461ced4a650f82dc7713581d730f88fe30c8831
dijidynamics <dijidynamics2024@gmail.com>
2025-01-26 16:20:19 +0800

    new project01  reactbootstrap

--- eventapp/vite.config.js
+++ eventapp/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Expose to all network interfaces
-    port: 5174, // You can specify a different port if needed
+    port: 5176, // You can specify a different port if needed
   },
 })
